9e70f85f248b381677a0489fa202bcfc52110b30
geihar <l0635968488@gmail.com>
2020-09-11 16:29:17 +0200

    final commit

--- mini_blog/settings.py
+++ mini_blog/settings.py
@@ -68,10 +68,21 @@ WSGI_APPLICATION = 'mini_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'NAME': os.path.join(BASE_DIR, 'users/db.sqlite3'),
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "pg",
+#         "PORT": 5432,
+#         "CONN_MAX_AGE": 0,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
